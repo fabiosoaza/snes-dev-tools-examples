599f8575655848c82722b8fff75f55835bc11a32
Fábio Souza <fabio.souza.eti@gmail.com>
2020-04-01 21:09:49 -0300

    Removing unnecessary build task

--- docker-local-build.py
+++ docker-local-build.py
@@ -5,8 +5,6 @@ import os
 
 def menu():
     parser = OptionParser()
-    parser.add_option("-b", "--build", 
-                      help="build local image", action="store_true")
     parser.add_option("-a", "--run-all-examples",
                      help="run all examples", action="store_true")
     parser.add_option("-i", "--run-it",
@@ -17,18 +15,14 @@ def menu():
 
     for option in vars(options):
         value =  getattr(options, option)
-        if option == 'build' and value == True:
-            build_image()
-        elif option == 'run_all_examples' and value == True:  
+        if option == 'run_all_examples' and value == True:  
             run_all_examples()      
         elif option == 'run_it' and value == True:  
             run_image()            
 
-def build_image():
-    os.system('docker build . -f Dockerfile -t fabiosoaza/snes-dev-tools:latest')    
 
 def run_image():
-    os.system('docker run -it -v $(pwd)/examples:/tmp/examples fabiosoaza/snes-dev-tools:latest bash')    
+    os.system('docker run -it -v $(pwd)/examples:/tmp/examples fabiosoaza/snes-dev-tools:v0.1.0 bash')    
 
 def run_all_examples():
     os.system('docker-compose up -d')
